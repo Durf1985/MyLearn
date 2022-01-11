c60c7b7ac0528c778133ad552e59fe1d2873bd1b
Durf1985 <durf19851@gmail.com>
2022-01-11 19:57:16 +0700

    Добавил пояснения к решению

--- 1.JavaSyntax/src/com/javarush/task/task10/task1015/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1015/Solution.java
@@ -8,25 +8,25 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
-        ArrayList<String>[] arrayOfStringList = createList();
+        ArrayList<String>[] arrayOfStringList = createList(); // Массив списков
         printList(arrayOfStringList);
     }
 
     public static ArrayList<String>[] createList() {
-        ArrayList<String>[] first = new ArrayList[10];
+        ArrayList<String>[] first = new ArrayList[10]; // 10 списков в массиве
         for (int i = 0; i < first.length; i++) {
-            ArrayList<String> second = new ArrayList<>();
+            ArrayList<String> second = new ArrayList<>(); //создание списка для массива
             for (int j = 0; j < first.length; j++) {
-                second.add("" + j);
+                second.add("" + j);// заполнение списка хламом
             }
-            first[i] = second;
+            first[i] = second; // добавление списка в массив
         }
         return first;
     }
 
     public static void printList(ArrayList<String>[] arrayOfStringList) {
-        for (ArrayList<String> list : arrayOfStringList) {
-            for (String s : list) {
+        for (ArrayList<String> list : arrayOfStringList) { //перебираем массив списков
+            for (String s : list) { // перебираем список
                 System.out.println(s);
             }
         }
